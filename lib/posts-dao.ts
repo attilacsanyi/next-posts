df5a0f82271107caa21ed041f36684705dcc50f3
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-23 17:15:57 +0100

    refactor: make getting post sql more readable

--- lib/posts-dao.ts
+++ lib/posts-dao.ts
@@ -12,13 +12,30 @@ export const getPosts = async (maxNumber?: number) => {
 
   // By design until there is no user switch: likes belongs to user id === 2 for now
   const stmt = db.prepare(`
-    SELECT posts.id, image_url AS image, title, content, created_at AS createdAt, first_name AS userFirstName, last_name AS userLastName, COUNT(likes.post_id) AS likes, EXISTS(SELECT * FROM likes WHERE likes.post_id = posts.id and likes.user_id = 2) AS isLiked
+    SELECT 
+      posts.id,
+      image_url AS image,
+      title,
+      content, 
+      created_at AS createdAt,
+      first_name AS userFirstName,
+      last_name AS userLastName,
+      COUNT(likes.post_id) AS likes,
+      EXISTS(
+        SELECT * 
+        FROM likes 
+        WHERE likes.post_id = posts.id 
+        AND likes.user_id = 2
+      ) AS isLiked
     FROM posts
-    INNER JOIN users ON posts.user_id = users.id
-    LEFT JOIN likes ON posts.id = likes.post_id
+    INNER JOIN users 
+      ON posts.user_id = users.id
+    LEFT JOIN likes 
+      ON posts.id = likes.post_id
     GROUP BY posts.id
     ORDER BY createdAt DESC
-    ${limitClause}`);
+    ${limitClause}
+  `);
 
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return (maxNumber ? stmt.all(maxNumber) : stmt.all()) as PostWithDetails[];
